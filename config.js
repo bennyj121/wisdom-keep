c926afe149c025f3ca96a51799b7cb0d4b59ea8a
bennyj121 <johnston121@gmail.com>
2026-07-19 14:00:18 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const CONFIG = {
   SUPABASE_URL: "https://wcefmxxhcitnwycofacp.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjZWZteHhoY2l0bnd5Y29mYWNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NzI1OTIsImV4cCI6MjA5OTQ0ODU5Mn0.zeMXAnq2K4MBb0W6aycMwRRlM3QI_CFyml9XYuqP0HE",
   FUNCTIONS_URL: "https://wcefmxxhcitnwycofacp.supabase.co/functions/v1",
-  SITE_URL: "https://bennyj121.github.io/wisdom-keep",
+  SITE_URL: "https://wisdomkeep.net",
 };
